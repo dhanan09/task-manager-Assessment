792d3f8351938eb81a67a66cb284762a047cd18e
dhanan09 <dhananjai921saxena@gmail.com>
2025-02-19 17:23:35 +0530

    3rd commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -51,7 +51,7 @@ exports.login = async (req, res) => {
             return res.status(400).json({ message: "Invalid credentials" });
         }
 
-        const token = jwt.sign({ id: user._id }, process.env.jwt_SECRET, { expiresIn: "1h" });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
         console.log("Generated Token:", token);
 
         res.status(200).json({ message: "Login successful", token });
